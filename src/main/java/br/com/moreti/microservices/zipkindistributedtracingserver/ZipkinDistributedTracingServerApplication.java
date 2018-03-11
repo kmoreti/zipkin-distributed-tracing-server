909f507916a40e3496d1ed922f66b339960d42c8
kmoreti <kleber.camargo@fatec.sp.gov.br>
2018-03-10 22:27:05 -0300

    Configured Zipkin Server.

--- src/main/java/br/com/moreti/microservices/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
+++ src/main/java/br/com/moreti/microservices/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
@@ -2,7 +2,9 @@ package br.com.moreti.microservices.zipkindistributedtracingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
 
+@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinDistributedTracingServerApplication {
 
