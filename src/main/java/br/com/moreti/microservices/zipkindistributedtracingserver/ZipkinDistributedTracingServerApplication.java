504e899a84f68e51b8640bccbc22c3e0a90cc099
kmoreti <kleber.camargo@fatec.sp.gov.br>
2018-03-15 22:04:09 -0300

    Configured eureka client.

--- src/main/java/br/com/moreti/microservices/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
+++ src/main/java/br/com/moreti/microservices/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
@@ -2,13 +2,16 @@ package br.com.moreti.microservices.zipkindistributedtracingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.EnableZipkinServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
-@EnableZipkinServer
 @SpringBootApplication
+@EnableZipkinStreamServer
+@EnableDiscoveryClient
 public class ZipkinDistributedTracingServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZipkinDistributedTracingServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinDistributedTracingServerApplication.class, args);
+    }
 }
+
